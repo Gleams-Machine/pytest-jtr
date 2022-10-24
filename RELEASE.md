9987b9636ec45a095ed27788ffd1d09578607682
Gleams API user <Stephen.Swannell+ghapi@gmail.com>
2022-10-24 13:41:25 +0100

    Include additional python versionsfor testing against

---
+++ RELEASE.md
@@ -0,0 +1,9 @@
+# Release Process
+
+## Preparation
+
+... is the key to success.
+
+- [x] All test pass: ```nox -s tests```
+- [x] All static code checks pass: ```nox -s static```
+- [x] Update version in pyproject.toml
--- noxfile.py
+++ noxfile.py
@@ -5,7 +5,7 @@ from nox_poetry import session
 nox.options.sessions = ["static", "tests"]
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def with_coverage(session):
     session.install(".")
     session.run(
@@ -15,50 +15,50 @@ def with_coverage(session):
     session.run('coverage', 'report', '-m', '--fail-under', '50')
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def unit_tests(session):
     session.install(".")
     session.run('pytest', 'tests', '-vvs', '-n', 'auto')
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def tests(session):
     session.notify("unit_tests")
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def flake8(session):
     session.run('flake8')
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def isort(session):
     session.run('isort', 'pytest_jtr')
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def bandit(session):
     session.run(
         'bandit', '--configfile', 'bandit.yml', '--recursive', 'pytest_jtr'
     )
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def safety(session):
     session.run('safety', 'check')
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def black(session):
     session.run('black', 'pytest_jtr')
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def mypy(session):
     session.run('mypy', '-p', 'pytest_jtr')
 
 
-@session(python=["3.9"])
+@session(python=["3.8", "3.9", "3.10"])
 def static(session):
     session.notify("isort")
     session.notify("flake8")
