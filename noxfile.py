2a016bcac44d63aa1736e7522811d1770d78c57e
Gleams99 <Stephen.Swannell@gmail.com>
2024-06-03 18:06:16 +0100

    Upgrade rich dep (#15)
    * Upgrade rich dep
    
    * py version rework
    
    * update to fix SCA
    
    ---------
    
    Co-authored-by: Gleams API user <Stephen.Swannell+ghapi@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -1,61 +1,16 @@
-import nox
 from nox_poetry import session
 
-nox.options.sessions = ["static", "tests"]
+py_versions = ["3.8", "3.9", "3.10"]
 
 
-@session(python=["3.8", "3.9", "3.10"])
+@session(python=py_versions)
 def with_coverage(session):
     session.install(".")
     session.run("coverage", "run", "-m", "pytest", "tests", "-vvs", "-n", "auto")
     session.run("coverage", "report", "-m", "--fail-under", "50")
 
 
-@session(python=["3.8", "3.9", "3.10"])
+@session(python=py_versions)
 def unit_tests(session):
     session.install(".")
     session.run("pytest", "tests", "-vvs", "-n", "auto")
-
-
-@session(python=["3.8", "3.9", "3.10"])
-def tests(session):
-    session.notify("unit_tests")
-
-
-@session(python=["3.8", "3.9", "3.10"])
-def flake8(session):
-    session.run("flake8")
-
-
-@session(python=["3.8", "3.9", "3.10"])
-def isort(session):
-    session.run("isort", "pytest_jtr")
-
-
-@session(python=["3.8", "3.9", "3.10"])
-def bandit(session):
-    session.run("bandit", "--configfile", "bandit.yml", "--recursive", "pytest_jtr")
-
-
-@session(python=["3.8", "3.9", "3.10"])
-def safety(session):
-    session.run("safety", "check", "--ignore=51457")
-
-
-@session(python=["3.8", "3.9", "3.10"])
-def black(session):
-    session.run("black", "pytest_jtr")
-
-
-@session(python=["3.8", "3.9", "3.10"])
-def mypy(session):
-    session.run("mypy", "-p", "pytest_jtr")
-
-
-@session(python=["3.8", "3.9", "3.10"])
-def static(session):
-    session.notify("isort")
-    session.notify("flake8")
-    session.notify("bandit")
-    session.notify("black")
-    session.notify("mypy")
