1bebf5d37b7ff3cca29b761f4bb41ff03443660a
Gleams API user <Stephen.Swannell+ghapi@gmail.com>
2024-06-03 23:21:17 +0100

    regen lock file

--- noxfile.py
+++ noxfile.py
@@ -4,13 +4,6 @@ py_versions = ["3.8", "3.9", "3.10", "3.11", "3.12"]
 
 
 @session(python=py_versions)
-def with_coverage(session):
-    session.install(".")
-    session.run("coverage", "run", "-m", "pytest", "tests", "-vvs", "-n", "auto")
-    session.run("coverage", "report", "-m", "--fail-under", "50")
-
-
-@session(python=py_versions)
 def unit_tests(session):
     session.install(".")
     session.run("pytest", "tests", "-vvs", "-n", "auto")
