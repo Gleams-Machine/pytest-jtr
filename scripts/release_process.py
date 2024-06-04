bb97be7117c6b59171e1f5087468d91c986e5509
Gleams99 <Stephen.Swannell@gmail.com>
2024-06-04 16:54:04 +0100

    release v1.3.0 (#17)
    Co-authored-by: Gleams API user <Stephen.Swannell+ghapi@gmail.com>

--- scripts/release_process.py
+++ scripts/release_process.py
@@ -15,7 +15,7 @@ def confirm_prompt(question: str) -> bool:
 def _run_tests():
     console.rule("[bold blue]Executing tests via nox")
     with console.status("[bold green] Test execution in progress..."):
-        cmd = ["nox", "-s", "tests"]
+        cmd = ["nox", "-s", "unit_tests"]
         result = subprocess.run(cmd, capture_output=True, text=True)
         if result.returncode:
             console.print(
@@ -26,23 +26,6 @@ def _run_tests():
             exit(result.returncode)
         else:
             console.print("[bold green] :green_heart: Tests have passed. Moving on...")
-
-
-def _run_static_checks():
-    console.rule("[bold blue]Executing static code analysis checks")
-    with console.status("[bold green] Static code checks in progress..."):
-        cmd = ["nox", "-s", "static"]
-        result = subprocess.run(cmd, capture_output=True, text=True)
-        if result.returncode:
-            console.print(result.returncode)
-            console.print(
-                "[bold red] :broken_heart: Checks have failed. Please investigate"
-            )
-            console.print(result.stdout)
-            console.print(result.stderr)
-            exit(result.returncode)
-        else:
-            console.print("[bold green] :green_heart: Checks have passed. Moving on...")
 
 
 def _run_release_process(release_type: str):
@@ -85,23 +68,19 @@ def upload_release():
 
 def prep_dev():
     _run_tests()
-    _run_static_checks()
     _run_release_process(release_type="prerelease")
 
 
 def prep_patch():
     _run_tests()
-    _run_static_checks()
     _run_release_process(release_type="patch")
 
 
 def prep_minor():
     _run_tests()
-    _run_static_checks()
     _run_release_process(release_type="minor")
 
 
 def prep_major():
     _run_tests()
-    _run_static_checks()
     _run_release_process(release_type="major")
